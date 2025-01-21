b4c345c542489beac78c6c49a2543ce24f344cb1
estebangalindo27 <estebangalindo27@gmail.com>
2025-01-21 08:35:23 -0500

    cracion de base de fatos y de clase peli

---
+++ examenp3/Servicios/BDS.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examenp3.Servicios
+{
+    public class BDS
+    {
+        private readonly SQLiteAsyncConnection _baseDeDatos;
+
+        public BDS(string rutaBD)
+        {
+            _baseDeDatos = new SQLiteAsyncConnection(rutaBD);
+            _baseDeDatos.CreateTableAsync<Peli>().Wait();
+        }
+
+        public Task<int> AgregarPeliculaAsync(Peli pelicula)
+        {
+            return _baseDeDatos.InsertAsync(pelicula);
+        }
+
+        public Task<List<Peli>> ObtenerPeliculasAsync()
+        {
+            return _baseDeDatos.Table<Peli>().ToListAsync();
+        }
+
+        public Task<int> EliminarTodasLasPeliculasAsync()
+        {
+            return _baseDeDatos.DeleteAllAsync<Peli>();
+        }
+    }
+}
