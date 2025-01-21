bb2a0c6353eedb6ba2f67d39926452f262ffd5e7
laboratorio <laboratorio@UPOAULA10316>
2025-01-21 07:24:44 -0500

    Agregar archivos de proyecto.

---
+++ examenp3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace examenp3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ examenp3/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace examenp3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
