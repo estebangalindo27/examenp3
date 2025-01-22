b19c2c6038f997fac5e78796a668d81a1c5c762b
estebangalindo27 <estebangalindo27@gmail.com>
2025-01-22 18:29:15 -0500

    estructura de una película con propiedades

--- examenp3/Models/Peli.cs
+++ examenp3/Models/Peli.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
-namespace examenp3.Models
+namespace ExamenP3.Modelos;
+
+public class Peli
 {
-    internal class Peli
-    {
-        public string Genero { get; set; }
-        public string Titulo { get; set; }
-        public string SitioWeb { get; set; }
-        public string Egalindo { get; set; }
-        public string Premios { get; set; }
-        public string ActorPrincipal { get; set; }
-        public int Id { get; set; }
-
-    }
-    
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public string ActorPrincipal { get; set; }
+    public string Premios { get; set; }
+    public string SitioWeb { get; set; }
+    public string EGalindo { get; set; }
 }
- 
